30fe0477a640eedea86cb4ec2e318eb656d1639b
IlyesBENAMARA <ilyesb2@gmail.com>
2022-10-04 17:35:15 +0100

    feat(partial): responsive chart container

--- packages/ez-vue/src/components/ResponsiveChartContainer.tsx
+++ packages/ez-vue/src/components/ResponsiveChartContainer.tsx
@@ -1,26 +1,14 @@
 import Vue, { PropType } from 'vue';
 import Component from 'vue-class-component';
 import { Prop, Watch, ProvideReactive } from 'vue-property-decorator';
-import {
-  ChartContext,
-  Dimensions,
-} from 'eazychart-core/src/types';
+import { Dimensions } from 'eazychart-core/src/types';
 import {
   debounce,
-  defaultChartContext,
   defaultChartDimensions,
 } from 'eazychart-core/src';
 
 @Component
 export default class ResponsiveChartContainer extends Vue {
-  @Prop({
-    type: Object as PropType<Dimensions>,
-    default() {
-      return {};
-    },
-  })
-  private readonly dimensions!: Partial<Dimensions>;
-
   private resizeObserver!: ResizeObserver;
 
   @Prop({
@@ -29,17 +17,12 @@ export default class ResponsiveChartContainer extends Vue {
   private readonly onResize?: (dimensions: Dimensions) => void;
 
   private containerDimensions: Dimensions = {
-    width: this.dimensions?.width || 0,
-    height: this.dimensions?.height || 0,
+    width: 0,
+    height: 0,
   };
 
-  private legendHeight = 0;
-
-  @ProvideReactive('responsiveChart')
-  private chart: ChartContext = {
-    ...defaultChartContext,
-    dimensions: this.chartDimensions,
-  };
+  @ProvideReactive('responsiveContainer')
+  private parentDimensions: Dimensions = this.containerDimensions
 
   beforeDestroy() {
     if (this.resizeObserver) {
@@ -49,39 +32,27 @@ export default class ResponsiveChartContainer extends Vue {
 
   created() {
     this.containerDimensions = {
-      width: this.dimensions?.width || defaultChartDimensions.width,
-      height: this.dimensions?.height || defaultChartDimensions.height,
+      width: this.parentDimensions?.width || defaultChartDimensions.width,
+      height: this.parentDimensions?.height || defaultChartDimensions.height,
     };
   }
 
   mounted() {
     // Dimensions values has not been set, we need to observe and resize
-    if (!this.dimensions?.width || !this.dimensions?.height) {
-      const observer = new ResizeObserver(
-        debounce((entries: ResizeObserverEntry[]) => {
-          this.resizeChart(entries);
-        }, 100),
-      );
-      const chartRef = this.$refs.chart as Node;
-      observer.observe(chartRef.parentNode as Element, {
-        box: 'border-box',
-      });
-    }
-  }
-
-  @Watch('dimensions')
-  onDimensionsChange() {
-    // If dimensions prop is provided, we force the values
-    const newDimensions = {
-      width: this.dimensions?.width || this.containerDimensions.width,
-      height: this.dimensions?.height || this.containerDimensions.height,
-    };
-    this.containerDimensions = newDimensions;
+    const observer = new ResizeObserver(
+      debounce((entries: ResizeObserverEntry[]) => {
+        this.resizeChart(entries);
+      }, 100),
+    );
+    const chartRef = this.$refs.responsiveChartContainer as Node;
+    observer.observe(chartRef as Element, {
+      box: 'border-box',
+    });
   }
 
   @Watch('containerDimensions')
   onContainerDimensionsChange() {
-    this.chart.dimensions = this.chartDimensions;
+    this.parentDimensions = this.containerDimensions;
   }
 
   resizeChart(entries: ResizeObserverEntry[]) {
@@ -91,11 +62,11 @@ export default class ResponsiveChartContainer extends Vue {
       // so that the end-user would be able to see the chart.
       const newDimensions = {
         width:
-          this.dimensions?.width
+          this.parentDimensions?.width
           || Math.floor(entry.contentRect.width)
           || defaultChartDimensions.width,
         height:
-          this.dimensions?.height
+          this.parentDimensions?.height
           || Math.floor(entry.contentRect.height)
           || defaultChartDimensions.height,
       };
@@ -104,38 +75,15 @@ export default class ResponsiveChartContainer extends Vue {
     });
   }
 
-  get chartDimensions(): Dimensions {
-    const { containerDimensions } = this;
-    // It take the container dimensions and subsctract padding and legend height
-    return {
-      width: containerDimensions.width,
-      height: containerDimensions.height,
-    };
-  }
-
-  get containerStyle() {
-    return {
-      width: `${this.containerDimensions.width}px`,
-      height: `${this.containerDimensions.height}px`,
-    };
-  }
-
-  get svgStyle() {
-    return {
-      width: `${this.containerDimensions.width}px`,
-      height: `${this.containerDimensions.height - this.legendHeight}px`,
-    };
-  }
-
   render() {
-    const { containerStyle } = this;
-
     const DefaultSlot = this.$scopedSlots.default
       ? this.$scopedSlots.default({})
       : null;
-
     return (
-      <div style={containerStyle} ref="responsiveChartContainer">
+      <div
+        style={{ height: '100%', width: '100%' }}
+        ref="responsiveChartContainer"
+      >
         {DefaultSlot}
       </div>
     );

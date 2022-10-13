49ec03adc2fa5cd48a97e25469f306d6585b2241
IlyesBENAMARA <ilyesb2@gmail.com>
2022-10-13 11:45:50 +0100

    feat: unit tests for multi area chart on react

---
+++ packages/ez-react/tests/unit/recipes/area/MultiAreaChart.spec.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { dimensions, pointsRawData } from 'eazychart-core/src/sample-data';
+import { act, render, RenderResult, waitFor } from '@testing-library/react';
+import 'tests/mocks/ResizeObserver';
+import { MultiAreaChart } from '@/recipes/area/MultiAreaChart';
+
+describe('MultiAreaChart', () => {
+  it('renders a multiarea chart', async () => {
+    let wrapper: RenderResult;
+    act(() => {
+      wrapper = render(
+        <MultiAreaChart
+          data={pointsRawData}
+          area={{
+            stroke: 'red',
+            strokeWidth: 2,
+          }}
+          marker={{
+            hidden: false,
+            color: 'blue',
+            radius: 2,
+          }}
+          grid={{ directions: [] }}
+          dimensions={dimensions}
+          yAxis={{
+            domainKeys: ['yValue', 'zValue'],
+          }}
+          animationOptions={{
+            easing: 'easeBack',
+            duration: 0,
+            delay: 0,
+          }}
+        />
+      );
+    });
+
+    await waitFor(() => {
+      expect(wrapper.container.innerHTML).toMatchSnapshot();
+    });
+  });
+});

4a25991755f3b82df6343d0b09afad9c30f9b676
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2022-10-23 18:39:09 +0100

    feat: poc svg animate

--- packages/ez-react/src/components/shapes/LinePath.tsx
+++ packages/ez-react/src/components/shapes/LinePath.tsx
@@ -1,6 +1,14 @@
-import React, { FC, SVGAttributes, useMemo } from 'react';
+import React, {
+  FC,
+  SVGAttributes,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { LineData, LineCurve } from 'eazychart-core/src/types';
 import { defaultColor, generateLinePath } from 'eazychart-core/src';
+import { useChart } from '@/lib/use-chart';
 
 export interface LinePathProps extends SVGAttributes<SVGPathElement> {
   shapeData?: LineData;
@@ -14,37 +22,60 @@ export const LinePath: FC<LinePathProps> = ({
   beta,
   stroke = defaultColor,
   strokeWidth = 1,
+  fill = 'none',
+  strokeLinejoin = 'round',
+  strokeLinecap = 'round',
+  children,
   ...rest
 }) => {
+  const {
+    animationOptions: { duration },
+  } = useChart();
   const dataPath = useMemo(
     () => generateLinePath(shapeData, curve, beta),
     [shapeData, curve, beta]
   );
+  const [pathLength, setPathLength] = useState(0);
+  const ref = useRef<SVGPathElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      setPathLength(ref.current.getTotalLength());
+    }
+  }, [dataPath]);
+
   return (
     <path
+      ref={ref}
       d={dataPath}
       stroke={stroke}
       strokeWidth={strokeWidth}
-      fill="none"
-      strokeLinejoin={'round'}
-      strokeLinecap={'round'}
-      strokeDasharray={1000}
+      fill={fill}
+      strokeLinejoin={strokeLinejoin}
+      strokeLinecap={strokeLinecap}
       {...rest}
       className="ez-line"
     >
-      <animate
-        attributeName="stroke-dashoffset"
-        attributeType="CSS"
-        from={1000}
-        to={0}
-        dur={'1s'}
-      />
-      <animate
-        attributeName="stroke-dasharray"
-        attributeType="CSS"
-        to={1000}
-        dur={'2s'}
-      />
+      {children ? (
+        children
+      ) : (
+        <>
+          <animate
+            attributeName="stroke-dasharray"
+            attributeType="CSS"
+            from={pathLength}
+            to={pathLength}
+            dur={'0s'}
+          />
+          <animate
+            attributeName="stroke-dashoffset"
+            attributeType="CSS"
+            from={pathLength}
+            to={0}
+            dur={`${duration}ms`}
+          />
+        </>
+      )}
     </path>
   );
 };
--- packages/ez-react/src/components/shapes/animate.tsx
+++
@@ -1,7 +0,0 @@
-<animate
-        attributeName="stroke-dashoffset"
-        attributeType="CSS"
-        from={1000}
-        to={0}
-        dur={'1s'}
-      />

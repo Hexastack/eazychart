06b431039f1cf16fa595fa91ae04dbdff4e39329
IlyesBENAMARA <ilyesb2@gmail.com>
2022-10-28 17:39:06 +0100

    feat(partial): stacked column chart (vue)

--- packages/ez-vue/src/components/scales/ColorScale.tsx
+++ packages/ez-vue/src/components/scales/ColorScale.tsx
@@ -27,12 +27,10 @@ export default class ColorScale extends Vue {
   created() {
     this.colorScale = this.defineScale();
     this.chart.registerScale('colorScale', this.colorScale);
-    // console.log('mounted', this.colorScale?.scale.domain());
   }
 
   updated() {
     this.colorScale = this.defineScale();
-    // console.log('hohaa', this.colorScale.scale.domain());
   }
 
   @Watch('chart.dimensions')
@@ -40,7 +38,6 @@ export default class ColorScale extends Vue {
   recomputeScale() {
     const { dimensions, data } = this.chart;
     this.colorScale.computeScale(dimensions, data);
-    // console.log('hehe', this.colorScale.scale.domain());
   }
 
   defineScale() {

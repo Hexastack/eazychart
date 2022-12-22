ea3473abb04d1a7b4e0ec6734f48b8a8f6b424f4
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2022-12-22 13:38:38 +0100

    fix: undo jest transform pattern

--- packages/ez-react/jest.config.js
+++ packages/ez-react/jest.config.js
@@ -1,24 +1,9 @@
 const baseConfig = require('eazychart-dev/jest/base.config');
 
-const esModules = [
-  'd3-scale',
-  'd3-array',
-  'internmap',
-  'd3-interpolate',
-  'd3-color',
-  'd3-format',
-  'd3-time',
-  'd3-shape',
-  'd3-path',
-  'd3-ease',
-  'd3-geo',
-].join('|');
-
 module.exports = {
   ...baseConfig,
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     'tests/(.*)': '<rootDir>/tests/$1',
   },
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 };

400512bc6afa4770f5f807dc87f2b37037cd0ec8
deathsh0ot <ala-hmz@outlook.com>
2023-03-07 16:36:11 +0100

    fix: smaller bubbles now appear on top

--- packages/ez-react/src/components/MapBubbles.tsx
+++ packages/ez-react/src/components/MapBubbles.tsx
@@ -43,6 +43,19 @@ export const MapBubbles: FC<MapBubblesProps> = ({
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data, geoJson?.features, geoDomainKey, colorScale.scale]);
+
+  // Add the value prop to the shapeDatums and sort them by it
+  // This helps render the highest value shapes (circles) first so the smaler bubbles would be on top
+
+  shapeData.forEach((shapeDatum) => {
+    const match = data.find((datumData) => datumData.id === shapeDatum.id);
+    if (match) {
+      (shapeDatum as any).value = match.value;
+    }
+  });
+
+  shapeData.sort((a, b) => (b as any).value - (a as any).value);
+
   return (
     <g {...rest} className="ez-map">
       {shapeData.map((shapeDatum, idx) => {

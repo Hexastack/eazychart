53792765ed6081261813a5608b42b4038fc07036
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-05-14 07:41:36 +0100

    fix: eazychart-react missing exports

--- packages/ez-react/src/index.ts
+++ packages/ez-react/src/index.ts
@@ -1,41 +1,52 @@
-export { ColumnChart, ColumnChartProps } from '@/recipes/column/ColumnChart';
-export { BarChart, BarChartProps } from '@/recipes/bar/BarChart';
-export { PieChart, PieChartProps } from '@/recipes/pie/PieChart';
-export { LineChart, LineChartProps } from '@/recipes/line/LineChart';
-export { BubbleChart, BubbleChartProps } from '@/recipes/scatter/BubbleChart';
+export { useAnimation } from '@/lib/use-animation';
+export { useChart } from '@/lib/use-chart';
+export { useMap } from '@/lib/use-map';
+export { useResponsiveChart } from '@/lib/use-responsive-chart';
+export { useToggableDatum } from '@/lib/useToggableDatum';
+export { useToggableDomainKey } from '@/lib/useToggableDomainKey';
+export { Legend, LegendProps } from '@/components/addons/legend/Legend';
+export { Tooltip, TooltipProps } from '@/components/addons/tooltip/Tooltip';
+export {
+  ResponsiveChartContainer,
+  ResponsiveChartContainerProps,
+} from '@/components/ResponsiveChartContainer';
 export { AreaChart, AreaChartProps } from '@/recipes/area/AreaChart';
+export {
+  MultiAreaChart,
+  MultiAreaChartProps,
+} from '@/recipes/area/MultiAreaChart';
+export { BarChart, BarChartProps } from '@/recipes/bar/BarChart';
+export { ColumnChart, ColumnChartProps } from '@/recipes/column/ColumnChart';
 export {
   LineColumnChart,
   LineColumnChartProps,
 } from '@/recipes/column/LineColumnChart';
-export {
-  SemiCircleChart,
-  SemiCircleChartProps,
-} from '@/recipes/pie/SemiCircleChart';
+export { LineChart, LineChartProps } from '@/recipes/line/LineChart';
 export {
   LineErrorMarginChart,
   LineErrorMarginChartProps,
 } from '@/recipes/line/LineErrorMarginChart';
 export {
-  IrregularPieChart,
-  IrregularPieChartProps,
-} from '@/recipes/pie/IrregularPieChart';
-export {
-  ScatterChart,
-  ScatterChartProps,
-} from '@/recipes/scatter/ScatterChart';
-export { Legend, LegendProps } from '@/components/addons/legend/Legend';
-export { RadialChart, RadialChartProps } from '@/recipes/pie/RadialChart';
-export {
   MultiLineChart,
   MultiLineChartProps,
 } from '@/recipes/line/MultiLineChart';
-export { MapChart, MapChartProps } from '@/recipes/map/MapChart';
 export {
   BubbleMapChart,
   BubbleMapChartProps,
 } from '@/recipes/map/BubbleMapChart';
+export { MapChart, MapChartProps } from '@/recipes/map/MapChart';
 export {
-  ResponsiveChartContainer,
-  ResponsiveChartContainerProps,
-} from '@/components/ResponsiveChartContainer';
+  IrregularPieChart,
+  IrregularPieChartProps,
+} from '@/recipes/pie/IrregularPieChart';
+export { PieChart, PieChartProps } from '@/recipes/pie/PieChart';
+export { RadialChart, RadialChartProps } from '@/recipes/pie/RadialChart';
+export {
+  SemiCircleChart,
+  SemiCircleChartProps,
+} from '@/recipes/pie/SemiCircleChart';
+export { BubbleChart, BubbleChartProps } from '@/recipes/scatter/BubbleChart';
+export {
+  ScatterChart,
+  ScatterChartProps,
+} from '@/recipes/scatter/ScatterChart';

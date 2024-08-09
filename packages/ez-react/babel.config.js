47c526762ec2d735e6b10bf84a38737380ba0264
Emnaghz <emnaghzaiel176@gmail.com>
2024-08-09 14:51:02 +0100

    add turbo changes

--- packages/ez-react/babel.config.js
+++ packages/ez-react/babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
   ],
   env: {
     test: {
